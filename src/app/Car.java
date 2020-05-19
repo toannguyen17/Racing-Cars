fe079d829482c0ec0f4c8599cbec2e5908a4e352
MrT98 <toan.vp98.wapmaster@gmail.com>
2020-05-19 11:25:41 +0700

    add project

---
+++ src/app/Car.java
@@ -0,0 +1,51 @@
+package app;
+
+import java.util.Random;
+
+public class Car extends Thread {
+    String name;
+    int    runDistance;
+    long   startTime;
+    public Car(String name){
+        this.name    = name;
+
+        // Khởi tạo điểm start(hay km ban đầu)
+        runDistance = 0;
+
+        // Khởi tạo time bắt đầu cuộc đua
+        startTime = System.currentTimeMillis();
+    }
+
+    @Override
+    public void run() {
+        // Kiểm tra chừng nào còn xe chưa kết thúc quãng đường đua thì xe vẫn tiếp tục chạy
+        while (runDistance < Main.DISTANCE) {
+            try {
+                // Random Speed KM/H
+                int speed = (new Random()).nextInt(20);
+                // Calculate traveled distance
+                runDistance += speed;
+                // Build result graphic
+                String log = "|";
+                int percentTravel = (runDistance * 100) / Main.DISTANCE;
+                for (int i = 0; i < Main.DISTANCE; i += Main.STEP) {
+                    if (percentTravel >= i + Main.STEP) {
+                        log += "=";
+                    } else if (percentTravel >= i && percentTravel < i + Main.STEP) {
+                        log += "o";
+                    } else {
+                        log += "-";
+                    }
+                }
+                log += "|";
+                System.out.println("Car" + this.name + ": " + log + " " + Math.min(Main.DISTANCE, runDistance) + "KM");
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Car" + this.name + " broken...");
+                break;
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Car" + this.name + " Finish in " + (endTime - startTime) / 1000 + "s");
+    }
+}
